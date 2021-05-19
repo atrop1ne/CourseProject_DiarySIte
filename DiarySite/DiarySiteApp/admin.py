43e6d03259fd81120288fc3e024fb5f1e511622b
Anton Tropin <antropy.n@gmail.com>
2021-05-19 20:33:54 +0700

    Новые модели и view, регистрация пользователей

--- DiarySite/DiarySiteApp/admin.py
+++ DiarySite/DiarySiteApp/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Account)
-admin.site.register(Category)
-admin.site.register(Event)
+admin.site.register(Note)
 admin.site.register(Tag)
--- DiarySite/DiarySiteApp/models.py
+++ DiarySite/DiarySiteApp/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Account(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo = models.ImageField('фото пользователя', null=True, blank=True, upload_to='profiles/%Y/%m/%d')
+
+    @receiver(post_save, sender=User)
+    def create_user_account(sender, instance, created, **kwargs):
+        try:
+            instance.account.save()
+        except ObjectDoesNotExist:
+            Account.objects.create(user=instance)
+
+
     class Meta:
         verbose_name = 'Аккаунт'
         verbose_name_plural = 'Аккаунты'
@@ -12,59 +25,44 @@ class Account(models.Model):
         return f"{self.user}"
 
     @property
-    def categories(self):
-        return Category.objects.filter(account = self)
-
-class Category(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    name = models.CharField('название категории', max_length=200, unique=True)
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-
-    def __str__(self):
-        return self.name
-
-    @property
-    def events(self):
-        return Event.objects.filter(category = self)
-
-    @property
     def tags(self):
-        return Tag.objects.filter(category = self)
+        return Tag.objects.filter(account = self)
+
+    @property
+    def notes(self):
+        return Note.objects.filter(account = self)
 
 class Tag(models.Model):
-    name = models.CharField('название тега', max_length=30, unique=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    title = models.CharField('название тега', max_length=30, unique=True)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
         abstract = False
     
-    def __str__(self):
-        return self.name
-
-class Event(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField('название события', max_length=30, null=True, blank=True)
-    description = models.TextField('описание события', null=True, blank=True)
+class Note(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    title = models.CharField('название заметки', max_length=30, null=True, blank=True)
+    description = models.TextField('описание заметки', null=True, blank=True)
     planned_date = models.DateTimeField('запланированная дата', null=True, blank=True)
     on_repeat = models.BooleanField('повторяется')
     to_remind = models.BooleanField('напоминание')
     remind_date = models.DateTimeField('дата напоминания', null=True, blank=True)
     on_delete = models.BooleanField('удалено', default=False)
     delete_date = models.DateField('дата удаления', null=True, blank=True, editable=False)
-    tag = models.ManyToManyField(Tag)
+    tag = models.ManyToManyField(Tag, blank=True)
 
     class Meta:
-        verbose_name = 'Событие'
-        verbose_name_plural = 'События'
+        verbose_name = 'Заметка'
+        verbose_name_plural = 'Заметки'
         abstract = False
 
     def __str__(self):
-        return f"Событие '{self.name}' категории '{self.category}'"
+        return f'Заметка "{self.title}"'
 
     @property
     def tags(self):

57e7f44c35387af72bf27cb39859d4b802b11039
Anton Tropin <antropy.n@gmail.com>
2021-05-22 17:38:41 +0700

    Манипуляции с заметками, корзина, стили

--- DiarySite/DiarySiteApp/urls.py
+++ DiarySite/DiarySiteApp/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.urls.conf import re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('main', views.main, name = 'main'),
+    re_path(r'^main', views.main, name = 'main'),
+    re_path(r'^noteform/(?P<id>\w+)$', views.noteView, name = 'noteform'),
+    re_path(r'^toTrashCan/(?P<id>\d+)$', views.toTrashCanNote, name = 'toTrashCan'),
+    path('trashCan', views.trashCanView, name = 'trashCan'),
+    path('clearTrashCan', views.clearTrashCan, name = 'clearTrashCan'),
+    re_path(r'^delete/(?P<id>\d+)$', views.deleteNote, name = 'delete'),
+    re_path(r'^restoreNote/(?P<id>\d+)$', views.restoreNote, name = 'restoreNote'),
 ]
--- DiarySite/DiarySiteApp/views.py
+++ DiarySite/DiarySiteApp/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
-
+import datetime
 
 def index(request):
     return render(request, 'DiarySiteApp/index.html', {'title' : 'Diary Site'})
@@ -9,7 +9,12 @@ def index(request):
 def main(request):
     user = User.objects.get(pk = request.user.pk)
     tags = user.account.tags
-    notes = user.account.notes
+    notes = Note.objects.filter(
+    account = user.account
+    ).filter(
+    on_delete = False
+    )
+
     photo = user.account.photo
     context ={
         'tags' : tags,
@@ -18,3 +23,69 @@ def main(request):
         'title' : 'Главная страница'
     }
     return render(request, 'DiarySiteApp/main.html', context)
+
+def noteView(request, id):
+    if id == 'add':
+        note = Note()
+        note.title = ''
+        note.description = ''
+        note.on_repeat = False
+        note.to_remind = False
+        user = User.objects.get(pk = request.user.pk)
+        note.account = user.account
+    else:
+        note = Note.objects.get(id = id)
+
+    if request.method == "POST":
+        note.title = request.POST.get("title")
+        note.description = request.POST.get("description")
+        note.save()
+        return redirect('main')
+    else:
+        return render(request, "DiarySiteApp/noteform.html", {"note": note, 'title': 'Ваша заметка'})
+
+def restoreNote(request, id):
+    note = Note.objects.get(id=id)
+    note.on_delete = False
+    note.save()
+    return redirect('trashCan')
+
+def toTrashCanNote(request, id):
+    note = Note.objects.get(id=id)
+    note.on_delete = True
+    note.delete_date = datetime.datetime.now().date()
+    note.save()
+    return redirect('main')
+
+def trashCanView(request):
+    user = User.objects.get(pk = request.user.pk)
+    tags = user.account.tags
+    notes = Note.objects.filter(
+    account = user.account
+    ).filter(
+    on_delete = True
+    )
+
+    photo = user.account.photo
+    context ={
+        'tags' : tags,
+        'notes' : notes,
+        'photo' : photo,
+        'title' : 'Корзина'
+    }
+    return render(request, 'DiarySiteApp/trashCan.html', context)
+
+def deleteNote(request, id):
+    note = Note.objects.get(id=id)
+    note.delete()
+    return redirect('trashCan')
+
+def clearTrashCan(request):
+    user = User.objects.get(pk = request.user.pk)
+    notes = Note.objects.filter(
+    account = user.account
+    ).filter(
+    on_delete = True
+    )
+    notes.delete()
+    return redirect('trashCan')
